e1a8feb33f2bd19d449e36c0152e88ac57609269
Alberto Bragagnolo <alberto.bragagnolo@cern.ch>
2024-11-29 14:41:47 +0100

    Synched Kstar to KPi selection cfi

--- BPHNano/python/KstarToKPi_cff.py
+++ BPHNano/python/KstarToKPi_cff.py
@@ -5,52 +5,47 @@ KstarToPiK = cms.EDProducer(
     'DiTrackBuilder',
     tracks = cms.InputTag('tracksBPH', 'SelectedTracks'),
     transientTracks = cms.InputTag('tracksBPH', 'SelectedTransientTracks'),
-    trk1Selection = cms.string(''),
-    trk2Selection = cms.string(''),
+    trk1Selection   = cms.string(''),
+    trk2Selection   = cms.string(''),
     trk1Mass = cms.double(0.139),
     trk2Mass = cms.double(0.494),
     preVtxSelection = cms.string('abs(userCand("l1").vz - userCand("l2").vz) <= 1.0 '
-                                 '&& mass() > 0.65 && mass()<1.24 && charge() == 0'),
-    postVtxSelection =  cms.string(
-         'userFloat("fitted_mass") >0.750 && userFloat("fitted_mass") < 1.04'
-         ' && userFloat("sv_prob")>0.01')
+                                 '&& mass() > 0.65 && mass() < 1.15'
+                                 '&& charge() == 0'),
+    postVtxSelection =  cms.string('userFloat("fitted_mass") > 0.750 && userFloat("fitted_mass") < 1.05'
+                                   '&& userFloat("sv_prob") > 0.01')
 )
 
 CountKstarPiK = cms.EDFilter("PATCandViewCountFilter",
     minNumber = cms.uint32(1),
     maxNumber = cms.uint32(999999),
-    src = cms.InputTag("KstarToPiK")
+    src       = cms.InputTag("KstarToPiK")
 )  
 
-
-
-
 KstarToPiKTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
-    src = cms.InputTag("KstarToPiK"),
-    cut = cms.string(""), #we should not filter on cross linked collections
+    src  = cms.InputTag("KstarToPiK"),
+    cut  = cms.string(""), #we should not filter on cross linked collections
     name = cms.string("KstarToPiK"),
     doc  = cms.string("slimmedMuons for BPark after basic selection"),
     singleton = cms.bool(False), # the number of entries is variable
     extension = cms.bool(False), # this is the main table for the muons
-    variables = cms.PSet(CandVars,
-          fitted_mass = Var("userFloat('fitted_mass')", float, doc="dxy/err (with sign) wrt first PV, in cm", precision=10),
-          fitted_pt = Var("userFloat('fitted_pt')", float, doc="dxy/err (with sign) wrt first PV, in cm", precision=10),
-          fitted_eta = Var("userFloat('fitted_eta')", float, doc="dxy/err (with sign) wrt first PV, in cm", precision=10),
-          fitted_phi = Var("userFloat('fitted_phi')", float, doc="dxy/err (with sign) wrt first PV, in cm", precision=10),
-          svprob = Var("userFloat('sv_prob')", float, doc="dxy/err (with sign) wrt first PV, in cm", precision=10),
-          trk1_mass = Var("userFloat('trk1_mass')", float, doc="dxy/err (with sign) wrt first PV, in cm", precision=10),
-          trk2_mass = Var("userFloat('trk2_mass')", float, doc="dxy/err (with sign) wrt first PV, in cm", precision=10),
-          trk1_idx = Var("userInt('trk1_idx')", int, doc="dxy/err (with sign) wrt first PV, in cm"),
-          trk2_idx = Var("userInt('trk2_idx')", int, doc="dxy/err (with sign) wrt first PV, in cm"),
-          second_mass_hypothesis = Var("userInt('second_mass_hypothesis')", int, doc="dxy/err (with sign) wrt first PV, in cm", precision=10),
-           vtx_x =Var("userFloat('vtx_x')", float, doc="dxy/err (with sign) wrt first PV, in cm", precision=10),
-          vtx_y = Var("userFloat('vtx_y')", float, doc="dxy/err (with sign) wrt first PV, in cm", precision=10),
-          vtx_z = Var("userFloat('vtx_z')", float, doc="dxy/err (with sign) wrt first PV, in cm", precision=10),
-
-          
+    variables = cms.PSet(
+        CandVars,
+        fitted_mass = Var("userFloat('fitted_mass')", float, doc="", precision=10),
+        fitted_pt   = Var("userFloat('fitted_pt')", float, doc="", precision=10),
+        fitted_eta  = Var("userFloat('fitted_eta')", float, doc="", precision=10),
+        fitted_phi  = Var("userFloat('fitted_phi')", float, doc="", precision=10),
+        svprob      = Var("userFloat('sv_prob')", float, doc="", precision=10),
+        trk1_mass   = Var("userFloat('trk1_mass')", float, doc="", precision=10),
+        trk2_mass   = Var("userFloat('trk2_mass')", float, doc="", precision=10),
+        trk1_idx    = Var("userInt('trk1_idx')", int, doc=""),
+        trk2_idx    = Var("userInt('trk2_idx')", int, doc=""),
+        second_mass_hypothesis = Var("userInt('second_mass_hypothesis')", int, doc="", precision=10),
+        vtx_x       = Var("userFloat('vtx_x')", float, doc="", precision=10),
+        vtx_y       = Var("userFloat('vtx_y')", float, doc="", precision=10),
+        vtx_z       = Var("userFloat('vtx_z')", float, doc="", precision=10),     
     )
 )
 
-
-KstarPiKSequence = cms.Sequence( KstarToPiK )
-KstarPiKTables = cms.Sequence(KstarToPiKTable)
+KstarPiKSequence = cms.Sequence(KstarToPiK)
+KstarPiKTables   = cms.Sequence(KstarToPiKTable)
