491177cd08e2d80a6453c54c4c2524b1512db8e4
Alberto Bragagnolo <alberto.bragagnolo@cern.ch>
2024-12-06 19:11:37 +0100

    python formatting

--- BPHNano/python/BToKstarLL_cff.py
+++ BPHNano/python/BToKstarLL_cff.py
@@ -24,43 +24,43 @@ BToKstarMuMu = cms.EDProducer(
 
 BToKstarMuMuTable = cms.EDProducer(
     'SimpleCompositeCandidateFlatTableProducer',
-    src = cms.InputTag("BToKstarMuMu"),
-    cut = cms.string(""),
-    name = cms.string("BToKstarMuMu"),
-    doc = cms.string("BToKstarMuMu Variables"),
-    singleton=cms.bool(False),
-    extension=cms.bool(False),
-    variables=cms.PSet(
+    src       = cms.InputTag("BToKstarMuMu"),
+    cut       = cms.string(""),
+    name      = cms.string("BToKstarMuMu"),
+    doc       = cms.string("BToKstarMuMu Variables"),
+    singleton = cms.bool(False),
+    extension = cms.bool(False),
+    variables = cms.PSet(
         # pre-fit quantities
         CandVars,
-        l1_idx = uint('l1_idx'),
-        l2_idx = uint('l2_idx'),
-        trk1_idx = uint('trk1_idx'),
-        trk2_idx = uint('trk2_idx'),
-        kstar_idx = uint('kstar_idx'),
-        trk1_mass = ufloat('trk1_mass'),
-        trk2_mass = ufloat('trk2_mass'),
-        min_dr = ufloat('min_dr'),
-        max_dr = ufloat('max_dr'),
+        l1_idx      = uint('l1_idx'),
+        l2_idx      = uint('l2_idx'),
+        trk1_idx    = uint('trk1_idx'),
+        trk2_idx    = uint('trk2_idx'),
+        kstar_idx   = uint('kstar_idx'),
+        trk1_mass   = ufloat('trk1_mass'),
+        trk2_mass   = ufloat('trk2_mass'),
+        min_dr      = ufloat('min_dr'),
+        max_dr      = ufloat('max_dr'),
         # vtx info
-        chi2 = ufloat('sv_chi2'),
-        svprob = ufloat('sv_prob'),
-        cos2D = ufloat('cos_theta_2D'),
+        chi2      = ufloat('sv_chi2'),
+        svprob    = ufloat('sv_prob'),
+        cos2D     = ufloat('cos_theta_2D'),
         fit_cos2D = ufloat('fitted_cos_theta_2D'),
-        l_xy = ufloat('l_xy'),
-        l_xy_unc = ufloat('l_xy_unc'),
+        l_xy      = ufloat('l_xy'),
+        l_xy_unc  = ufloat('l_xy_unc'),
         # post-fit momentum /masses
-        mll_fullfit = ufloat('fitted_mll'),
+        mll_fullfit    = ufloat('fitted_mll'),
         mkstar_fullfit = ufloat('fitted_ditrack_mass'),
-        fit_mass = ufloat('fitted_mass'),
-        fit_massErr = ufloat('fitted_massErr'),
-        fit_pt = ufloat('fitted_pt'),
-        fit_eta = ufloat('fitted_eta'),
-        fit_phi = ufloat('fitted_phi'),
+        fit_mass       = ufloat('fitted_mass'),
+        fit_massErr    = ufloat('fitted_massErr'),
+        fit_pt         = ufloat('fitted_pt'),
+        fit_eta        = ufloat('fitted_eta'),
+        fit_phi        = ufloat('fitted_phi'),
         # vertex
-        vtx_x = ufloat('vtx_x'),
-        vtx_y = ufloat('vtx_y'),
-        vtx_z = ufloat('vtx_z'),
+        vtx_x   = ufloat('vtx_x'),
+        vtx_y   = ufloat('vtx_y'),
+        vtx_z   = ufloat('vtx_z'),
         vtx_cxx = ufloat('vtx_cxx'),
         vtx_cyy = ufloat('vtx_cyy'),
         vtx_czz = ufloat('vtx_czz'),
@@ -85,8 +85,8 @@ BToKstarMuMuTable = cms.EDProducer(
         fit_trk2_eta = ufloat('fitted_trk2_eta'),
         fit_trk2_phi = ufloat('fitted_trk2_phi'),
         # isolation 
-        l1_iso04 = ufloat('l1_iso04'),
-        l2_iso04 = ufloat('l2_iso04'),
+        l1_iso04   = ufloat('l1_iso04'),
+        l2_iso04   = ufloat('l2_iso04'),
         trk1_iso04 = ufloat('trk1_iso04'),
         trk2_iso04 = ufloat('trk2_iso04'),
         # constraint properties
@@ -95,22 +95,19 @@ BToKstarMuMuTable = cms.EDProducer(
         constraint_massErr  = ufloat('constraint_massErr'),
         constraint_mll      = ufloat('constraint_mll'),
 
-        trk1_svip2d = ufloat('trk1_svip2d'),
+        trk1_svip2d     = ufloat('trk1_svip2d'),
         trk1_svip2d_err = ufloat('trk1_svip2d_err'),
-        trk2_svip2d = ufloat('trk2_svip2d'),
+        trk2_svip2d     = ufloat('trk2_svip2d'),
         trk2_svip2d_err = ufloat('trk2_svip2d_err'),
     )
 )
 
-
 CountBToKstarMuMu = cms.EDFilter("PATCandViewCountFilter",
     minNumber = cms.uint32(1),
     maxNumber = cms.uint32(999999),
-    src = cms.InputTag("BToKstarMuMu")
+    src       = cms.InputTag("BToKstarMuMu")
 )
-
 
 ########################### Sequencies  ############################
 BToKstarMuMuSequence = cms.Sequence( BToKstarMuMu  )
-BToKstarMuMuTables = cms.Sequence( BToKstarMuMuTable )
-
+BToKstarMuMuTables   = cms.Sequence( BToKstarMuMuTable )
